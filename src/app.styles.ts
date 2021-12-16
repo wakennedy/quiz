37b51bf78c19f6f1f6d1ceb7bf4cf7c9eae92875
wakennedy <alexkenned@gmail.com>
2021-12-16 09:48:34 -0800

    img import

--- src/app.styles.ts
+++ src/app.styles.ts
@@ -1,2 +1,8 @@
 import styled, { createGlobalStyle } from 'styled-components';
 import BGImage from './images/nattu-adnan.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    height: 100%;
+}
+`
